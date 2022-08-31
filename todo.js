f7dc7b708da358c7d0341297a8c6db3061683c92
MitaliChotalia <106459663+MitaliChotalia@users.noreply.github.com>
2022-08-31 11:06:31 +0530

    Create todo.js

---
+++ todo.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", function() { 
+    const input = document.querySelector('input');
+    const btn = document.querySelector('.addTask > button');
+    console.log(input);
+
+    btn.addEventListener('click' , addList);
+    input.addEventListener('keyup',(event) => {
+        (event.keycode === 13 ? addList(event) : null);
+    })
+
+    function addList(event){
+        const notCompleted = document.querySelector('.notCompleted');
+        const Completed = document.querySelector('.Completed');
+
+        const newLi = document.createElement('li');
+        const checkBtn = document.createElement('button');
+        const delBtn = document.createElement('button');
+
+        checkBtn.innerHTML = '<i class = "fa fa-check"></i>';
+        delBtn.innerHTML = '<i class = "fa fa-trash"></i>';
+
+        if(input.value !== ''){
+            newLi.textContent = input.value;
+            input.value = '';
+            notCompleted.appendChild(newLi);
+            newLi.appendChild(checkBtn);
+            newLi.appendChild(delBtn);
+        }
+
+        checkBtn.addEventListener('click',function(){
+            const parent = this.parentNode;
+            parent.remove();
+            Completed.appendChild(parent);
+            checkBtn.style.display = 'none'
+        });
+
+        delBtn.addEventListener('click',function(){
+            const parent = this.parentNode;
+            parent.remove();
+        });
+    }
+});
